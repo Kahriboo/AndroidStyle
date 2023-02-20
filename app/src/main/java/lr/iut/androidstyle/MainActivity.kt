be7b4d22ada56cb5d90725bc4bb991352e4d2085
lenn <lenn.angel@externe.maif.fr>
2023-02-20 18:57:06 +0100

    initial commit

---
+++ app/src/main/java/lr/iut/androidstyle/MainActivity.kt
@@ -0,0 +1,11 @@
+package lr.iut.androidstyle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
